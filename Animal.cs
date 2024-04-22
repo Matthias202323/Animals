e8d020d98f381c9d7924892adca7c35a2c85692c
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-04-22 09:11:05 +0200

    Ajoutez des fichiers projet.

---
+++ Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public virtual void Eat() { }
+        public virtual void Move() { }
+    }
+
+    public class Man:Animal
+    {
+        private int _pattes = 2;
+        public override void Eat() {
+            System.Console.WriteLine($"I eat...I have {_pattes} legs");
+        }
+        public override void Move() {
+            System.Console.WriteLine("I move");
+        }
+    }
+    public class Monkey : Animal
+    {
+        private int _pattes = 2;
+        public override void Eat() {
+            System.Console.WriteLine($"I eat...I have {_pattes} legs");
+        }
+        public override void Move() { }
+    }
+    public class Elephant : Animal
+    {
+        private int _pattes = 4;
+        public override void Eat() {
+            System.Console.WriteLine($"I eat...I have {_pattes} legs");
+        }
+        public override void Move() { }
+    }
+    public class Turtle : Animal
+    {
+        private int _pattes = 4;
+        public override void Eat() {
+            System.Console.WriteLine($"I eat...I have {_pattes} legs");
+        }
+        public override void Move() { }
+    }
+    public class Lizard : Animal
+    {
+        private int _pattes = 4;
+        public override void Eat() {
+            System.Console.WriteLine($"I eat...I have {_pattes} legs");
+        }
+        public override void Move() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Animals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Man man = new Man();
+            Monkey monkey = new Monkey();
+            Lizard  lizard = new Lizard();
+            Turtle turtle = new Turtle();
+        }
+    }
+}
