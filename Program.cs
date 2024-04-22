593070badd34bb2d0ebb33f678726f77342d6af9
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-04-22 09:15:01 +0200

    program

--- Program.cs
+++ Program.cs
@@ -5,9 +5,17 @@
         static void Main(string[] args)
         {
            Man man = new Man();
+            man.Eat();
+            man.Move();
             Monkey monkey = new Monkey();
+            monkey.Eat();
+            monkey.Move();
             Lizard  lizard = new Lizard();
+            lizard.Eat();
+            lizard.Move();
             Turtle turtle = new Turtle();
+            turtle.Eat();
+            turtle.Move();
         }
     }
 }
